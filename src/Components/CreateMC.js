3b40833b51590ac944732874fa434d2c2d414109
Meagan <meagan.h.smith@gmail.com>
2022-10-28 13:28:15 -0400

    Add submission on enter

--- src/Components/CreateMC.js
+++ src/Components/CreateMC.js
@@ -1,7 +1,59 @@
 import React, { useState } from 'react';
 
 function CreateMC() {
+    let [question, setQuestion] = useState("Add poll question");
+    let [showResponseBuidler, setShowResponseBuilder] = useState(false);
+    let [responses, setResponses] = useState([]);
+    
+    function handleEnter(e) {
+        if (e.key === "Enter") {
+            setQuestion(e.target.value)
+        }
 
+        console.log("New question: ", question)
+    }
+
+    function handleEnterRes(e) {
+        if (e.key === "Enter") {
+            setResponses(responses => [...responses, e.taget.innerHTML])
+            console.log(responses);
+        }
+    }
+
+    function showResponseBuilder() {
+        setShowResponseBuilder(true);
+    }
+
+    return (
+        <div>
+            <h1>Multiple Choice</h1>
+
+            <input
+            type="text"
+            placeholder={ question }
+            onKeyDown={ handleEnter }
+            >
+            
+            </input>
+
+            <br></br>
+
+            <button onClick={ showResponseBuilder }>Add Response</button>
+
+            {
+                showResponseBuidler &&
+                <div>
+                    <br></br>
+                    <input
+                    placeholder='Add response text'
+                    onKeyDown={ handleEnterRes }
+                    >
+                    </input>
+                </div>
+            }
+        </div>
+
+    )
 
 }
 
